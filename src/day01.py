245bb9adc66b46085c8af410b9c6a400e6049596
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2022-12-02 08:02:34 +0100

    Minor changes

--- src/day01.py
+++ src/day01.py
@@ -2,5 +2,18 @@ from utils.api import get_input
 
 input_str = get_input(1)
 
-# WRITE YOUR SOLUTION HERE
+elves = []
+calories = 0
 
+for l in input_str:
+    if l == '\n':
+        elves.append(calories)
+        calories = 0
+    else:
+        calories += int(l)
+
+elves.sort(reverse=True)
+
+print(elves[0])
+
+print(sum(elves[:3]))
--- src/day02.py
+++ src/day02.py
@@ -2,81 +2,88 @@ from utils.api import get_input
 
 input_str = get_input(2)
 
-# A for Rock, B for Paper, and C for Scissors
-# X for Rock, Y for Paper, and Z for Scissors
 
-def outcome(a, b):
-    if(a == "A" and b == "X"):
-        return 3
-    if(a == "A" and b == "Y"):
-        return 6
-    if(a == "A" and b == "Z"):
-        return 0
-
-    if(a == "B" and b == "X"):
-        return 0
-    if(a == "B" and b == "Y"):
-        return 3
-    if(a == "B" and b == "Z"):
-        return 6
-
-    if(a == "C" and b == "X"):
-        return 6
-    if(a == "C" and b == "Y"):
-        return 0
-    if(a == "C" and b == "Z"):
-        return 3
-
-
-# A for Rock, B for Paper, and C for Scissors
-#  X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
-def followrule(a, b):
-    if(a == "A" and b == "X"):
-        return "Z"
-    if(a == "A" and b == "Y"):
-        return "X"
-    if(a == "A" and b == "Z"):
-        return "Y"
-
-    if(a == "B" and b == "X"):
-        return "X"
-    if(a == "B" and b == "Y"):
-        return "Y"
-    if(a == "B" and b == "Z"):
-        return "Z"
-
-    if(a == "C" and b == "X"):
-        return "Y"
-    if(a == "C" and b == "Y"):
-        return "Z"
-    if(a == "C" and b == "Z"):
-        return "X"
+# Opponent: A for Rock, B for Paper, and C for Scissors
+# Me:       X for Rock, Y for Paper, and Z for Scissors
+# Score:    0 if you lost, 3 if the round was a draw, and 6 if you won
+def score_match(a, b):
+    if a == "A":
+        match b:
+            case "X":
+                return 3
+            case "Y":
+                return 6
+            case "Z":
+                return 0
+    if a == "B":
+        match b:
+            case "X":
+                return 0
+            case "Y":
+                return 3
+            case "Z":
+                return 6
+    if a == "C":
+        match b:
+            case "X":
+                return 6
+            case "Y":
+                return 0
+            case "Z":
+                return 3
 
 
+# Opponent: A for Rock, B for Paper, and C for Scissors
+# Strategy: X: lose, Y: draw, Z: win
+def get_response(a, b):
+    if a == "A":
+        if b == "X":
+            return "Z"
+        if b == "Y":
+            return "X"
+        if b == "Z":
+            return "Y"
+
+    if a == "B":
+        if b == "X":
+            return "X"
+        if b == "Y":
+            return "Y"
+        if b == "Z":
+            return "Z"
+
+    if a == "C":
+        if b == "X":
+            return "Y"
+        if b == "Y":
+            return "Z"
+        if b == "Z":
+            return "X"
+
+
+# 1 for Rock, 2 for Paper, and 3 for Scissors
 def score_shape(a):
-    if(a == "X"):
+    if a == "X":
         return 1
-    if(a == "Y"):
+    if a == "Y":
         return 2
-    if(a == "Z"):
+    if a == "Z":
         return 3
 
-total_score = 0
-
-for l in input_str:
-    opponent = l.split()[0]
-    response = l.split()[1]
-    total_score += (score_shape(response) + outcome(opponent, response))
-
-print(total_score)
-#  shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
 
 total_score = 0
+for game in input_str:
+    opponent = game.split()[0]
+    response = game.split()[1]
+    total_score += (score_shape(response) + score_match(opponent, response))
 
-for l in input_str:
-    opponent = l.split()[0]
-    order = l.split()[1]
-    response = followrule(opponent, order)
-    total_score += (score_shape(response) + outcome(opponent, response))
+print("Total score with interpretation  I of the secret strategy guide: " + str(total_score))
 
-print(total_score)
+total_score = 0
+for game in input_str:
+    opponent = game.split()[0]
+    order = game.split()[1]
+    response = get_response(opponent, order)
+    total_score += (score_shape(response) + score_match(opponent, response))
+
+print("Total score with interpretation II of the secret strategy guide: " + str(total_score))
