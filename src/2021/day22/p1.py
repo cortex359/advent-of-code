b44be8a135a8bc43b9ed78a026dc6ea5df94cdf1
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2022-12-05 05:55:09 +0100

    WIP

--- src/2021/day22/p1.py
+++ src/2021/day22/p1.py
@@ -3,7 +3,7 @@ import numpy as np
 with open("input") as file:
 	data = [line.removesuffix("\n") for line in file]
 
-ranges: list[list[bool, range, range, range]] = []
+ranges: list[list[bool, int, int, int, int, int, int]] = []
 for seq in data:
 	state = 1 if seq.split()[0] == "on" else 0
 	for dimension in seq.split()[1].split(","):
@@ -16,12 +16,56 @@ for seq in data:
 		elif dimension.startswith("z="):
 			z_from = int(dimension[2:].split("..")[0])
 			z_to = int(dimension[2:].split("..")[1])
-	ranges.append([state, range(x_from, x_to), range(y_from, y_to), range(z_from, z_to)])
+	ranges.append([state, x_from, x_to, y_from, y_to, z_from, z_to])
 
 print(ranges[0])
 
-# Parsing
+
+def contains(r: list[bool, int, int, int, int, int, int], s: list[bool, int, int, int, int, int, int]):
+	a_1, a_2, b_1, b_2, c_1, c_2 = r[1:]
+	x_1, x_2, y_1, y_2, z_1, z_2 = s[1:]
+	if a_2 <= x_2 and a_1 >= x_1:
+		if b_2 <= y_2 and b_1 >= y_1:
+			if c_2 <= z_2 and c_1 >= z_1:
+				return True
+
+def intersects(r: list[bool, int, int, int, int, int, int], s: list[bool, int, int, int, int, int, int]):
+	a_1, a_2, b_1, b_2, c_1, c_2 = r[1:]
+	x_1, x_2, y_1, y_2, z_1, z_2 = s[1:]
+	if a_1 <= x_1 <= a_2 or a_1 <= x_2 <= a_2:
+		if b_1 <= y_1 <= b_2 or b_1 <= y_2 <= b_2:
+			if c_1 <= z_1 <= c_2 or c_1 <= z_2 <= c_2:
+				return True
+
+def intersection(r: list[bool, int, int, int, int, int, int], s: list[bool, int, int, int, int, int, int]):
+	a_1, a_2, b_1, b_2, c_1, c_2 = r[1:]
+	x_1, x_2, y_1, y_2, z_1, z_2 = s[1:]
+	if x_1 <= a_1 <= x_2:
+		if x_1 <= a_2 <= x_2:
+
+
+
+
+def volume(r: list[bool, int, int, int, int, int, int]):
+	x_1, x_2, y_1, y_2, z_1, z_2 = r[1:]
+	if r[0]:
+		sign = 1
+	else:
+		sign = -1
+	return (x_2 - x_1) * (y_2 - y_1) * (z_2 - z_1) * sign
+
+
+contain_counter = 0
 for r in ranges:
+	intersect_counter = 0
+	for s in ranges:
+		if r != s:
+			if contains(r, s):
+				contain_counter += 1
+			if intersects(r, s):
+				intersect_counter += 1
+	if intersect_counter == 22:
+		print(r)
+	print(intersect_counter)
 
-
-print(cube.sum())
+print(contain_counter)
