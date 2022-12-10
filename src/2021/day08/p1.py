b7d835736558fe42b3a5e9ff9a27ca330ee7d1f9
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2022-12-10 10:53:33 +0100

    wip

--- src/2021/day08/p1.py
+++ src/2021/day08/p1.py
@@ -2,17 +2,28 @@ with open("input") as file:
 	data = [line.removesuffix("\n") for line in file]
 
 # display | segments
-#       1 |       cf
-#       7 |      acf
-#       4 |     bcdf
-#       2 |    acdeg
-#       3 |    acdfg
-#       5 |    abdfg
-#       0 |   abcefg
-#       6 |   abdefg
-#       9 |   abcdfg
-#       8 |  abcdefg
+#       1 |       cf |    c  f
+#       7 |      acf |  a c  f
+#       4 |     bcdf |   bcd f
+#       2 |    acdeg |  a cde g
+#       3 |    acdfg |  a cd fg
+#       5 |    abdfg |  ab d fg
+#       0 |   abcefg |  abc efg
+#       6 |   abdefg |  ab defg
+#       9 |   abcdfg |  abcd fg
+#       8 |  abcdefg |  abcdefg
 
 for line in data:
 	patterns, output = line.split(" | ")
-	[x for x in patters.split() if len(x) <= 4]
+	figures = patterns.split()
+
+	one:      set = set(c for c in [x for x in figures if len(x) == 2][0])
+	seven:    set = set(c for c in [x for x in figures if len(x) == 3][0])
+	four:     set = set(c for c in [x for x in figures if len(x) == 4][0])
+	eight:    set = set(c for c in [x for x in figures if len(x) == 7][0])
+	# 2, 3, 5
+	length_5: list[set] = [y for z in [x for x in patterns.split() if len(x) == 5] for y in set(c for c in y)
+	# 0, 6, 9
+	length_6: list[set] = set(x for x in patterns.split() if len(x) == 6)
+
+	print(one.intersection(seven, four))
