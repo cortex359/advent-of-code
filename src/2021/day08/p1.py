3763b1ff188b118f261e9213e4969837e0e8831b
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2022-12-10 05:51:26 +0100

    start 2021-08

--- src/2021/day08/p1.py
+++ src/2021/day08/p1.py
@@ -1,5 +1,18 @@
 with open("input") as file:
 	data = [line.removesuffix("\n") for line in file]
 
+# display | segments
+#       1 |       cf
+#       7 |      acf
+#       4 |     bcdf
+#       2 |    acdeg
+#       3 |    acdfg
+#       5 |    abdfg
+#       0 |   abcefg
+#       6 |   abdefg
+#       9 |   abcdfg
+#       8 |  abcdefg
+
 for line in data:
-	
+	patterns, output = line.split(" | ")
+	[x for x in patters.split() if len(x) <= 4]
