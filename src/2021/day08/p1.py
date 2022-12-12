20bd6dedede66c8676ef3139ca5fbefde8a22c1b
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2022-12-12 05:52:51 +0100

    wip

--- src/2021/day08/p1.py
+++ src/2021/day08/p1.py
@@ -13,6 +13,19 @@ with open("input") as file:
 #       9 |   abcdfg |  abcd fg
 #       8 |  abcdefg |  abcdefg
 
+# default_figs: dict = {
+# 	"1": "cf",
+# 	"7": "acf",
+# 	"4": "bcdf",
+# 	"2": "acdeg",
+# 	"3": "acdfg",
+# 	"5": "abdfg",
+# 	"0": "abcefg",
+# 	"6": "abdefg",
+# 	"9": "abcdfg",
+# 	"8": "abcdefg"
+# }
+
 for line in data:
 	patterns, output = line.split(" | ")
 	figures = patterns.split()
@@ -22,8 +35,15 @@ for line in data:
 	four:     set = set(c for c in [x for x in figures if len(x) == 4][0])
 	eight:    set = set(c for c in [x for x in figures if len(x) == 7][0])
 	# 2, 3, 5
-	length_5: list[set] = [y for z in [x for x in patterns.split() if len(x) == 5] for y in set(c for c in y)
+	length_5: list[set] = []
+	for c in [x for x in figures if len(x) == 5]:
+		length_5.append(set(y for y in c))
 	# 0, 6, 9
-	length_6: list[set] = set(x for x in patterns.split() if len(x) == 6)
+	length_6: list[set] = []
+	for c in [x for x in figures if len(x) == 6]:
+		length_6.append(set(y for y in c))
+
+	a: str = [x for x in seven.difference(one)][0]
+	
 
 	print(one.intersection(seven, four))
