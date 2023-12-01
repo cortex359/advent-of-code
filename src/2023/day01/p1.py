b53c479129ae64d69914f22c7d2707a33e29dbb0
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2023-12-01 10:44:03 +0100

    :flushed:

--- src/2023/day01/p1.py
+++ src/2023/day01/p1.py
@@ -3,40 +3,13 @@ import re
 with open("input") as file:
     data = [line.removesuffix("\n") for line in file]
 
-summe = 0
-
-def first_num(line):
-    d = 1
-    rank = []
-
-    for word in ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'):
-        if line.find(word) >= 0:
-            rank.append((word, d, line.find(word)))
-        d += 1
-
-    rank = sorted(rank, key=lambda x: x[2], reverse=False)
-    if len(rank) == 0:
-        return re.sub(r'\D', '', line)[0]
-    return re.sub(r'\D', '', re.sub(rank[0][0], str(rank[0][1]), line))[0]
-
-
-def last_num(line):
-    d = 1
-    rank = []
-
-    for word in ('one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'):
-        if line.rfind(word) >= 0:
-            rank.append((word, d, line.rfind(word)))
-        d += 1
-
-    rank = sorted(rank, key=lambda x: x[2], reverse=True)
-    if len(rank) == 0:
-        return re.sub(r'\D', '', line)[-1]
-    return re.sub(r'\D', '', re.sub(r'(.*)'+rank[0][0], str(rank[0][1]), line))[-1]
-
+sum_cvs = 0
 
 for line in data:
-    print('{} → {} + {}'.format(line, first_num(line), last_num(line)))
-    summe += int(str(first_num(line)) + str(last_num(line)))
+    cleand_line: str = re.sub(r'\D', '', line)
+    calibration_value: int = int(cleand_line[0] + cleand_line[-1])
+    print('{} → {} : {}'.format(line, cleand_line, calibration_value))
+    sum_cvs += calibration_value
 
-print(summe)
+# 54331
+print(sum_cvs)
--- src/2023/day01/p2.py
+++ src/2023/day01/p2.py
@@ -2,3 +2,46 @@ import re
 
 with open("input") as file:
     data = [line.removesuffix("\n") for line in file]
+
+number_words = {1: 'one',
+                2: 'two',
+                3: 'three',
+                4: 'four',
+                5: 'five',
+                6: 'six',
+                7: 'seven',
+                8: 'eight',
+                9: 'nine'}
+summe = 0
+
+
+def first_num(line):
+    rank = []
+    for d, word in number_words.items():
+        if line.find(word) >= 0:
+            rank.append((word, d, line.find(word)))
+
+    rank = sorted(rank, key=lambda x: x[2])
+    if len(rank) == 0:
+        return re.sub(r'\D', '', line)[0]
+    return re.sub(r'\D', '', re.sub(rank[0][0], str(rank[0][1]), line))[0]
+
+
+def last_num(line):
+    rank = []
+    for d, word in number_words.items():
+        if line.rfind(word) >= 0:
+            rank.append((word, d, line.rfind(word)))
+
+    rank = sorted(rank, key=lambda x: x[2], reverse=True)
+    if len(rank) == 0:
+        return re.sub(r'\D', '', line)[-1]
+    return re.sub(r'\D', '', re.sub(r'(.*)' + rank[0][0], str(rank[0][1]), line))[-1]
+
+
+for line in data:
+    print('{} → {} + {}'.format(line, first_num(line), last_num(line)))
+    summe += int(str(first_num(line)) + str(last_num(line)))
+
+# 54518
+print(summe)
