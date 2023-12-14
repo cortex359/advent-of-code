f7ebe9ce659d0ab57f7f838acb24da3e39385603
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2023-12-14 23:06:36 +0100

    Improved error handling with zsh script

--- src/2015/day08/p1.py
+++ src/2015/day08/p1.py
@@ -1,184 +1,28 @@
 import re
-import numpy as np
-from collections import deque
-from collections import defaultdict
-import itertools
-
-# d = defaultdict(int)  # Default to int (0)
 
 with open("input") as file:
-    data: list = [line.removesuffix("\n") for line in file]
+    data: list[str] = [line.removesuffix("\n") for line in file]
 
-# Parsing Stuff
-# -------------
-number_list: list[int] = [int(line) for line in data if line.isdigit()]
-grid: list[list] = [list(line) for line in data]
+chars_of_string_code = 0
+chars_in_memory = 0
 
-# Regex Stuff
-# -----------
-def line_to_list_of_ints(line):
-    """Convert a line of numbers to a list of ints"""
-    return list(map(int, re.findall(r'\d+', line)))
+for line in data:
+    chars_in_memory += len(line)
+    escaped_string = re.sub(r'(^"|"$)', '', line)
+    escaped_string = re.sub(r'\\\\|\\"|\\x[0-9a-f]{2}', 'x', escaped_string)
+    chars_of_string_code += len(escaped_string)
 
+print(f"chars in memory: {chars_in_memory}")
+print(f"chars of string code: {chars_of_string_code}")
+print(f"difference: {chars_in_memory - chars_of_string_code}")
 
-def get_fullstr_by_pointing_to_segment(line: str, index: int, chars: str = "01234567890", valid: bool = True) -> str:
-    """Get full string of valid chars by pointing to a segment of it, expending left and right from there"""
-    end = index
+chars_escaped = 0
+for line in data:
+    escaped_string = re.sub(r'\\', r'\\\\', line)
+    escaped_string = re.sub(r'"', '\\"', escaped_string)
+    escaped_string = re.sub(r'^(.*)$', r'"\1"', escaped_string)
+    print(escaped_string)
+    chars_escaped += len(escaped_string)
 
-    while index >= 0 and ((chars.find(line[index]) >= 0 and valid) or (chars.find(line[index]) < 0 and not valid)):
-        index -= 1
-    while end < len(line) and ((chars.find(line[end]) >= 0 and valid) or (chars.find(line[end]) < 0 and not valid)):
-        end += 1
-
-    return line[index + 1:end]
-
-
-# cartesian product
-# for i, j in itertools.product('ABC', 'xyz'):
-#   print('{}{}, '.format(i, j), end='')
-# Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz
-
-# permutations
-# itertools.permutations('ABCD', 2)
-# AB AC AD BA BC BD CA CB CD DA DB DC
-
-# combinations
-# itertools.combinations('ABCD', 2)
-# AB AC AD BC BD CD
-
-
-# 2D Grid Stuff
-# -------------
-def get_neighbor_coordinates(grid, zeile: int, spalte: int) -> list[tuple[int, int]]:
-    """Get all cross neighbor coordinates in a 2D Grid"""
-    neighbors = []
-    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        nx, ny = zeile + dx, spalte + dy
-        if 0 <= nx < len(grid[0]) and 0 <= ny < len(grid):
-            neighbors.append((nx, ny))
-    return neighbors
-
-
-def get_neighbors(grid, zeile: int, spalte: int) -> list[tuple]:
-    """Get all cross neighbor values in a 2D Grid"""
-    neighbors = []
-    for _x, _y in get_neighbor_coordinates(grid, zeile, spalte):
-        neighbors.append(grid[_x][_y])
-    return neighbors
-
-
-def get_diagonal_coordinates(grid, zeile, spalte):
-    """Get all diagonal neighbor coordinates in a 2D Grid"""
-    neighbors = []
-    for dx, dy in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
-        nx, ny = zeile + dx, spalte + dy
-        if 0 <= nx < len(grid[0]) and 0 <= ny < len(grid):
-            neighbors.append((nx, ny))
-    return neighbors
-
-
-def get_diagonals(grid, zeile, spalte):
-    """Get all diagonal neighbor values in a 2D Grid"""
-    neighbors = []
-    for _x, _y in get_diagonal_coordinates(grid, zeile, spalte):
-        neighbors.append(grid[_x][_y])
-    return neighbors
-
-
-def rotate_grid(grid):
-    """Rotate a 2D Grid 90 degrees clockwise"""
-    return [list(reversed(col)) for col in zip(*grid)]
-
-
-# 3D Grid Stuff
-# -------------
-def create_3d_grid(depth, height, width, fill_value=0):
-    """Create a 3D Grid with a default value"""
-    return [[[fill_value for _ in range(width)] for _ in range(height)] for _ in range(depth)]
-
-
-def get_3d_neighbor_coordinates(grid, x, y, z):
-    """Get all valid neighbor coordinates (up to 26) in a 3D Grid"""
-    neighbors = []
-    for dx, dy, dz in [(i, j, k) for i in [-1, 0, 1] for j in [-1, 0, 1] for k in [-1, 0, 1] if (i, j, k) != (0, 0, 0)]:
-        nx, ny, nz = x + dx, y + dy, z + dz
-        if 0 <= nx < len(grid[0][0]) and 0 <= ny < len(grid[0]) and 0 <= nz < len(grid):
-            neighbors.append((nx, ny, nz))
-    return neighbors
-
-def get_3d_neighbors(grid, x, y, z):
-    """Get all valid neighbor values (up to 26) in a 3D Grid"""
-    neighbors = []
-    for _x, _y, _z in get_3d_neighbor_coordinates(grid, x, y, z):
-        neighbors.append(grid[_z][_y][_x])
-    return neighbors
-
-# Graph Stuff
-# -----------
-def create_graph(edges: list[tuple]) -> dict:
-    """Create a Graph from a list of Edges"""
-    graph: dict = {}
-    for a, b in edges:
-        if a not in graph:
-            graph[a] = []
-        if b not in graph:
-            graph[b] = []
-        graph[a].append(b)
-        graph[b].append(a)  # Omit this for directed graphs
-    return graph
-
-
-def dfs(graph: dict, start, visited=None):
-    """Depth-First Search (DFS)"""
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    for neighbor in graph[start]:
-        if neighbor not in visited:
-            dfs(graph, neighbor, visited)
-    return visited
-
-
-def bfs(graph, start):
-    """Breadth-First Search (BFS)"""
-    visited, queue = set(), deque([start])
-    while queue:
-        vertex = queue.popleft()
-        if vertex not in visited:
-            visited.add(vertex)
-            queue.extend(set(graph[vertex]) - visited)
-    return visited
-
-
-def shortest_path(graph, start, end):
-    """Shortest Path between two nodes in a Graph"""
-    queue = deque([(start, [start])])
-    visited = set()
-    while queue:
-        vertex, path = queue.popleft()
-        if vertex == end:
-            return path
-        for neighbor in graph[vertex]:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.append((neighbor, path + [neighbor]))
-    return None
-
-
-def has_cycle(graph, start):
-    """Check if a Graph has a cycle"""
-    visited, stack = set(), [(start, None)]
-    while stack:
-        node, parent = stack.pop()
-        if node in visited:
-            return True
-        visited.add(node)
-        for neighbor in graph[node]:
-            if neighbor != parent:
-                stack.append((neighbor, node))
-    return False
-
-
-def manhattan_distance(x1, y1, x2, y2):
-    """Manhattan Distance between two points (x1, y1) and (x2, y2)"""
-    return abs(x1 - x2) + abs(y1 - y2)
+print(f"encoded length: {chars_escaped}")
+print(f"difference: {chars_escaped - chars_in_memory}")
--- src/utils/new.zsh
+++ src/utils/new.zsh
@@ -1,9 +1,24 @@
 #!/usr/bin/env zsh
 
-AOC_DIR="${HOME}/Projekte/advent.of.code/advent-of-code"
+AOC_DIR="${${PWD%/utils}%/src}"
 TEMPLATE="${AOC_DIR}/src/utils/template.py"
+AOC_CLI_BIN="$(whence aoc)"
 
-AOC_CLI_BIN="${HOME}/.cargo/bin/aoc"
+if [[ "${${${PWD%/utils}%/src}:t}" != "advent-of-code" ]]; then {
+    print -Pu2 "%F{red}[ERROR] AOC_DIR is expected to point to a dir named %Badvent-of-code%b but was %B${AOC_DIR}%b.%f"
+    print -Pu2 "%F{red}Aborting.%f"
+    exit 1
+}; fi
+if [[ ! -s ${TEMPLATE} ]]; then {
+    print -Pu2 "%F{red}[ERROR] TEMPLATE=%B${TEMPLATE}%b not found or empty.%f"
+    exit 2
+}; fi
+if [[ ! -x ${AOC_CLI_BIN} ]]; then {
+    print -Pu2 "%F{red}[ERROR] aoc-cli was not found.%f"
+    exit 3
+}; fi
+
+
 YEAR=${YEAR:=$(date +%Y)}
 DAY=${DAY:=$(date +%d)}
 
@@ -19,58 +34,65 @@ target_seconds=$(date --date "${YEAR}-12-${DAY} 00:00 UTC-5" +%s)
 ###
 ### Wait for the target time
 ###
-print -P "%F{blue}Target: ${target_seconds}%f"
+if (( target_seconds > $(date +%s) )); then {
+    print -P "🎄 %F{blue}Waiting until $(date -d @${target_seconds} +'%A, den %d. %B %Y, %X Uhr %Z')%f"
 
-while : ; do {
-  current_seconds=$(date +%s)
-  (( current_seconds + 3 > target_seconds )) && break
-  echo -en "\r$(date +%s.%N)"
-  sleep 0.5
-}; done
+    ### Waiting phase 1
+    while : ; do {
+        current_seconds=$(date +%s)
+        time_left=$(( target_seconds - current_seconds ))
+        (( time_left < 3 )) && break
+        print -P "\r %F{blue}%B${time_left}%b seconds%f"
+        sleep 0.5
+    }; done
 
-print -P "\n\n%F{red}%BGet ready!!!%b%f"
+    ### Waiting phase 2
+    print -P "\n\n%F{red}%BGet ready!!!%b%f"
+    while (( $(date +%s) <=  target_seconds )); do {
+        current_seconds=$(date +%s)
+        (( current_seconds >= target_seconds )) && break
+        echo -en "\r$(date +%s.%N)"
+        sleep .01
+    }; done
+}; fi
 
-while (( $(date +%s) <=  target_seconds )); do {
-  current_seconds=$(date +%s)
-  (( current_seconds >= target_seconds )) && break
-  echo -en "\r$(date +%s.%N)"
-  sleep .01
-}; done
 
-print -P "\n\n%F{blue}Starting at %B$(date +%s.%N)%b%f\n"
+print -P "\n\n%F{blue}Starting at %B$(date +'%A, den %d. %B %Y, %X Uhr %Z. %s.%N')%b%f\n"
 
 ###
 ### Download the puzzle
 ###
-
 max_error_counter=0
-while (( max_error_counter < 5 )); do {
-  if ${AOC_CLI_BIN} download --year "$YEAR" --day "$DAY" --overwrite ; then {
-    print -P "%F{green}Puzzle downloaded%f"
-    break
-  }; else {
-    print -P "%F{red}Error downloading puzzle%f"
-    (( max_error_counter++ ))
-    sleep .1
-  }; fi
+max_errors=5
+while (( max_error_counter <= max_errors )); do {
+    if ${AOC_CLI_BIN} download --year "${YEAR}" --day "${DAY}" --overwrite ; then {
+        print -P "%F{green}[OK] Puzzle downloaded.%f"
+        break
+    }; else {
+        waiting_for=$(( 0.1 + max_error_counter * 0.2 ))
+        (( max_error_counter++ ))
+        print -Pu2 "%F{red}[ERROR] Could not download puzzle. ${max_error_counter}/${max_errors}%f"
+        print -Pu2 "%F{red}[ERROR] Waiting for %B${waiting_for}%b seconds before trying again.%f"
+        sleep ${waiting_for}
+    }; fi
 }; done
 
-if (( max_error_counter >= 5 )); then {
-  print -P "%F{red}Too many errors. Exiting.%f"
-  exit 1
+if (( max_error_counter >= max_errors )); then {
+    print -Pu2 "%F{red}[ERROR] Oh fuck! Too many errors. Exiting.%f"
+    exit 4
 }; fi
 
 if pcre2grep -Me '(?:[`]{3}[\n\s]*)([^`]+)(?:[`]{3})' -m1 --output '$1' "${work_dir}/puzzle.md" ; then {
-  print -P "%F{green}Example found%f"
+    print -P "%F{green}Example found%f"
 
-  print -P "%F{blue}Extracting example:%f"
-  pcre2grep -Me '(?:[`]{3}[\n\s]*)([^`]+)(?:[`]{3})' -m1 --output '$1' \
-    "${work_dir}/puzzle.md" >| "${work_dir}/example"
+    print -P "%F{blue}Extracting example:%f"
+    pcre2grep -Me '(?:[`]{3}[\n\s]*)([^`]+)(?:[`]{3})' -m1 --output '$1' \
+        "${work_dir}/puzzle.md" >| "${work_dir}/example"
 
-  print -P "%F{blue}Example:%f"
-  cat "${work_dir}/example"
+    print -P "%F{blue}Example:%f"
+    cat "${work_dir}/example"
 }; else {
-  print -P "%F{red}No example found%f"
+    print -P "%F{red}No example found%f"
 }; fi
 
-print -P "\n%F{green}Have fun!%f"
+print -P "\n🎄 %F{green}Have fun!%f 🎄"
