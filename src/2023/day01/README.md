c36d350f0acbdcf8cd67d80e75ce6f05eada0c5f
Christian Rene Thelen <christian.thelen@rwth-aachen.de>
2023-12-18 05:38:37 +0100

    Day17 wip...

---
+++ src/2023/day01/README.md
@@ -0,0 +1,17 @@
+# Day 1: Trebuchet?!
+
+In the heart of a snow-covered village, the Christmas elves are in a flurry of activity. Their faces, a blend of determination and joy, are framed by pointed hats dusted with frost. The village is aglow with twinkling lights and garlands, but the usually bustling workshop stands still, the air tense with concern. The elves' mission is clear: solve the puzzle to save Christmas.
+
+The workshop is a treasure trove of festive decorations and tools, with candy cane-striped screwdrivers and holly-embellished hammers. One elf, her cheeks rosy and eyes narrowed in concentration, examines a piece of parchment, the amended calibration document, her fingers tracing the lines of text and whimsical drawings made by a young elf.
+
+Her companions gather around, their pointed ears twitching as they ponder over the clues. They work together, their small hands moving deftly as they combine traditional methods with magical tools—a compass that always points to the North Star, a magnifying glass that reveals the hidden, and an abacus with beads shaped like miniature snowflakes.
+
+A breakthrough comes when an elf, with a eureka moment evident in his wide-eyed expression, realizes that some digits are spelled out in letters. The team's frustration turns to exhilaration, their earlier tension melting away like snow on a sunny day.
+
+They recalibrate, their nimble fingers dancing over the document. As they sum up the true calibration values, the village seems to breathe a collective sigh of relief. The elves share high-fives and hugs, their laughter ringing through the crisp winter air, as the workshop roars back to life, ready to restore snow operations and keep the spirit of Christmas alive.
+
+![img_01.png](img_01.png)
+![img_02.png](img_02.png)
+![img_03.png](img_03.png)
+![img_04.png](img_04.png)
+![img_05.png](img_05.png)
--- src/2023/day17/p1.py
+++ src/2023/day17/p1.py
@@ -8,84 +8,96 @@ import heapq
 with open("example") as file:
     data: list = [line.removesuffix("\n") for line in file]
 
-grid: list[list] = [list(line) for line in data]
 
 def get_neighbor_coordinates(grid, zeile: int, spalte: int) -> list[tuple[int, int]]:
     """Get all cross neighbor coordinates in a 2D Grid"""
     neighbors = []
-    for dx, dy in [(-1, 0), (1, 0), (1, 0), (0, 1)]:
+    for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
         nm, nn = zeile + dx, spalte + dy
-        if 0 <= nm < len(grid[0]) and 0 <= nn < len(grid):
+        if 0 <= nm < len(grid) and 0 <= nn < len(grid[0]):
             neighbors.append((nm, nn))
     return neighbors
 
 
-def get_neighbors(grid, zeile: int, spalte: int) -> list[tuple]:
-    """Get all cross neighbor values in a 2D Grid"""
-    neighbors = []
-    for _x, _y in get_neighbor_coordinates(grid, zeile, spalte):
-        neighbors.append(grid[_x][_y])
-    return neighbors
-
-
-def get_diagonal_coordinates(grid, zeile, spalte):
-    """Get all diagonal neighbor coordinates in a 2D Grid"""
-    neighbors = []
-    for dx, dy in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
-        nx, ny = zeile + dx, spalte + dy
-        if 0 <= nx < len(grid[0]) and 0 <= ny < len(grid):
-            neighbors.append((nx, ny))
-    return neighbors
-
-
-def get_diagonals(grid, zeile, spalte):
-    """Get all diagonal neighbor values in a 2D Grid"""
-    neighbors = []
-    for _x, _y in get_diagonal_coordinates(grid, zeile, spalte):
-        neighbors.append(grid[_x][_y])
-    return neighbors
-
-
-
-heat_lost = 0
-position = (0, 0)
-def heuristic(a, b):
-    return abs(b[0] - a[0]) + abs(b[1] - a[1])
+def manhattan_distance(a, b):
+    """Manhattan Distance between two points (x1, y1) and (x2, y2)"""
+    x1, y1 = a
+    x2, y2 = b
+    return abs(x1 - x2) + abs(y1 - y2)
 
 
 def a_star(grid, start, goal):
+    """
+    Implementation of the A* search algorithm.
+
+    Parameters:
+    grid (list): A 2D list representing the grid.
+    start (tuple): A tuple representing the starting coordinates.
+    goal (tuple): A tuple representing the goal coordinates.
+
+    Returns:
+    came_from (dict): A dictionary mapping each node to the node it came from.
+    cost_so_far (dict): A dictionary mapping each node to the cost of the path from the start node to it.
+    """
+
+    # Initialize the frontier with the start node
     frontier = []
     heapq.heappush(frontier, (0, start))
-    came_from: dict[tuple[int, int], None] = {start: None}
-    cost_so_far: dict[tuple[int, int], int] = {start: 0}
-    directions: dict[tuple[int, int], list[tuple[int, int]]] = {start: []}
 
+    # Initialize the came_from and cost_so_far dictionaries
+    came_from = {start: None}
+    cost_so_far = {start: 0}
+
+    # Initialize the directions dictionary
+    directions = {start: []}
+
+    # Loop until the frontier is empty
     while frontier:
+        # Pop the node with the lowest cost from the frontier
         _, current = heapq.heappop(frontier)
 
+        # If the current node is the goal, we're done
         if current == goal:
             break
 
+        # For each neighbor of the current node
         for next in get_neighbor_coordinates(grid, current[0], current[1]):
-            # ↓(1, 0) ←(0, -1) ↑(-1, 0) →(0, 1)
+            # Calculate the direction to the neighbor
             new_direction = (next[0] - current[0], next[1] - current[1])
+
+            # Add the new direction to the list of directions for the current node
             new_directions = directions[current] + [new_direction]
 
-            # no more than 3 steps in one direction
+            # If there are more than 3 steps in the same direction, skip this neighbor
             if len(new_directions) >= 3 and all(d == new_direction for d in new_directions[-3:]):
                 continue
-            # no 180° turns
-            if len(new_directions) >= 1 and sum(new_directions[-1]) + sum(new_direction) == 0:
+
+            # If the new direction is a 180° turn, skip this neighbor
+            if len(new_directions) >= 1 and new_directions[-1][0] + new_direction[0] == 0 and new_directions[-1][1] + \
+                    new_direction[1] == 0:
                 continue
 
+            # Calculate the cost of the path to the neighbor through the current node
             new_cost = cost_so_far[current] + int(grid[next[0]][next[1]])
+
+            # If the neighbor hasn't been visited yet or the new cost is lower than the previous cost
             if next not in cost_so_far or new_cost < cost_so_far[next]:
+                # Update the cost of the path to the neighbor
                 cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
+
+                # Calculate the priority of the neighbor
+                priority = new_cost + manhattan_distance(goal, next)
+
+                # Add the neighbor to the frontier with its priority
                 heapq.heappush(frontier, (priority, next))
+
+                # Update the node the neighbor came from
                 came_from[next] = current
+
+                # Update the directions to the neighbor
                 directions[next] = new_directions
 
+    # Return the came_from and cost_so_far dictionaries
     return came_from, cost_so_far
 
 def reconstruct_path(came_from, start, goal):
@@ -107,6 +119,7 @@ def display_grid(grid) -> None:
             print(e, end="")
         print("")
 
+
 def display_path_in_grid(grid, path) -> None:
     """Display a 2D Grid"""
     print("\nGrid:")
@@ -119,10 +132,17 @@ def display_path_in_grid(grid, path) -> None:
         print("")
 
 
-
-start, goal = (0, 0), (len(grid)-1, len(grid[0])-1)
+grid = [list(map(int, line)) for line in data]
+start, goal = (0, 0), (len(grid) - 1, len(grid[0]) - 1)
 print(f"Start: {start}, Goal: {goal}")
+
+# A*
 came_from, cost_so_far = a_star(grid, start, goal)
+
+# Reconstruct Path
 path = reconstruct_path(came_from, start, goal)
 
 display_path_in_grid(grid, path)
+
+# Print the total heat loss
+print(cost_so_far[goal])
